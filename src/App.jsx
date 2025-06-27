1212f0d0dc517196536ba6d8ff32a5723ac6479a
Atul Kumar <atulkumar.works@gmail.com>
2025-06-27 12:54:17 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+function App() {
+
+return (
+  <>
+
+  <div className="w-1/2 h-40 flex justify-center mt-10 ml-10 bg-amber-500">
+   <div className="w-1/3 h-24 mt-10 ml-10 bg-blue-300">
+  </div>
+  </div>
+
+
+  </>
+)
+ 
+}
+
+export default App
